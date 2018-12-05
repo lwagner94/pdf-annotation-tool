01738f44ac57bd021765e9183124ccc0c461d815
Lukas Wagner <lwagner94@posteo.at>
2018-12-05 19:58:02 +0100

    Add simple overview over uploaded files in index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const models = require('../db/models');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    models.Document.find((err, documents) => {
+        if (err) {
+            res.code(500).send("DB Error");
+        }
+
+        res.render('index', {title: 'Express', documents: documents});
+    });
+
 });
 
 router.get('/upload', (req, res, next) => {
-  res.render('upload');
+    res.render('upload');
 });
 
 module.exports = router;
