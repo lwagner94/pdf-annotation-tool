65501dec93ae7810cc8a5b1677b3be56ba28591a
Lukas Wagner <lwagner94@posteo.at>
2018-12-10 19:24:46 +0100

    Add database models for Annotation and AnnotationSet

--- backend/db/models.js
+++ backend/db/models.js
@@ -8,6 +8,18 @@ const documentSchema = new mongoose.Schema({
     size: Number
 });
 
-const Document = mongoose.model("Document", documentSchema);
+const annotationSetSchema = new mongoose.Schema({
+    documentID: String,
+    userID: mongoose.Schema.Types.ObjectId,
+    locked: Boolean
+});
 
-module.exports.Document = Document;
+const annotationSchema = new mongoose.Schema({
+    setID: mongoose.Schema.Types.ObjectId,
+    pageNumber: Number,
+    properties: String,
+});
+
+module.exports.Document = mongoose.model("Document", documentSchema);
+module.exports.AnnotationSet = mongoose.model("AnnotationSet", annotationSetSchema);
+module.exports.Annotation = mongoose.model("Annotation", annotationSchema);
