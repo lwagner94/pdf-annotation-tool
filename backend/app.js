95c08517675e06a9b30bbb88f09455100ee9a590
Lukas Wagner <lwagner94@posteo.at>
2018-12-13 13:37:26 +0100

    Move viewer to different folder

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/viewer', express.static('../viewer/dist'));
+app.use('/viewer', express.static('./public/frontend/dist'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
