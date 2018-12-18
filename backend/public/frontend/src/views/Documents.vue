eb4fe237071b671125ed0d5deb0353d967b20b86
Lukas Wagner <lwagner94@posteo.at>
2018-12-18 18:40:45 +0100

    Add document uploading and deletion

--- backend/public/frontend/src/views/Documents.vue
+++ backend/public/frontend/src/views/Documents.vue
@@ -1,32 +1,111 @@
 <template>
     <div>
-        <ul>
-            <li v-for="document in documents" :key="document.id">
-                <div>
-                    <img :src="'/api/documents/' + document.id + '/thumb'">
-                    <router-link :to="{ name: 'viewer', query: { document: document.id }}">{{document.name}}</router-link>
+        <div>
+            <ul>
+                <li v-for="document in documents" :key="document.id">
+                    <div>
+                        <img :src="'/api/documents/' + document.id + '/thumb'">
+                        <router-link :to="{ name: 'viewer', query: { document: document.id }}">{{document.name}}</router-link>
+                        <button @click="showDeletionDialog(document)">Delete document</button>
+                    </div>
+
+                </li>
+            </ul>
+        </div>
+
+        <button @click="showUploadDialog()">Upload</button>
+
+        <!-- The Modal -->
+        <div v-show="modalVisible" ref="modal" class="modal">
+
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close" @click="setVisible(false)">&times;</span>
+                <div v-show="uploadVisible">
+                    <input ref="fileField" type="file" accept="application/pdf">
+                    <button @click="uploadDocument">Upload</button>
+                </div>
+                <div v-show="deleteVisible">
+                    <span>Are you sure?</span>
+                    <button @click="deleteDocument()">Yes</button>
+                    <button @click="setVisible(false)">No</button>
                 </div>
 
-            </li>
-        </ul>
+
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script>
+
     export default {
         name: "Documents",
 
         data() {
             return {
-                documents: []
+                documents: [],
+                modalVisible: false,
+                uploadVisible: false,
+                deleteVisible: false,
+                document: undefined
             }
         },
         mounted()  {
-            fetch("/api/documents")
-                .then(response => response.json())
-                .then(response => {
-                    this.documents = response;
-                });
+            this.getDocuments();
+        },
+
+        methods: {
+            getDocuments() {
+                fetch("/api/documents")
+                    .then(response => response.json())
+                    .then(response => {
+                        this.documents = response;
+                    });
+            },
+
+            deleteDocument() {
+                fetch("/api/documents/" + this.document.id, {
+                    method: "DELETE",
+                }).then(response => {
+                    this.document = undefined;
+                    this.getDocuments();
+                    this.setVisible(false);
+                })
+            },
+
+
+            uploadDocument() {
+                const fileField = this.$refs.fileField;
+                const formData = new FormData();
+                formData.append("pdf", fileField.files[0]);
+
+                fetch("/api/documents", {
+                    method: "POST",
+                    body: formData
+                }).then(response => {
+                    this.setVisible(false);
+                    this.getDocuments();
+                })
+            },
+
+            setVisible(visible) {
+                this.modalVisible = visible;
+            },
+
+            showDeletionDialog(document) {
+                this.document = document;
+                this.deleteVisible = true;
+                this.uploadVisible = false;
+                this.setVisible(true);
+            },
+
+            showUploadDialog() {
+                this.uploadVisible = true;
+                this.deleteVisible = false;
+                this.setVisible(true);
+            }
         }
     }
 </script>
@@ -34,5 +113,42 @@
 <style scoped lang="less">
     img {
         height: 200px;
+    }
+
+    .modal {
+        /*display: none; !* Hidden by default *!*/
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    }
+
+    /* Modal Content/Box */
+    .modal-content {
+        background-color: #fefefe;
+        margin: 15% auto; /* 15% from the top and centered */
+        padding: 20px;
+        border: 1px solid #888;
+        width: 80%; /* Could be more or less, depending on screen size */
+    }
+
+    /* The Close Button */
+    .close {
+        color: #aaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+    }
+
+    .close:hover,
+    .close:focus {
+        color: black;
+        text-decoration: none;
+        cursor: pointer;
     }
 </style>
